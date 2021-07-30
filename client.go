dc76d7836c930499ad368a726e6cd194295e6f72
Alex <kudrushow@gmail.com>
2021-07-30 12:44:42 +0300

    fix: version

--- client.go
+++ client.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-const PackageVersion = "v0.0.1"
+const PackageVersion = "v0.0.2"
 const DefaultUserAgent = "CamundaClientGo/" + PackageVersion
 const DefaultEndpointUrl = "http://localhost:8080/engine-rest"
 const DefaultTimeoutSec = 60
